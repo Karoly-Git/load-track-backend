b0fafeb97ee3ea04728e13ad3dccace8d5ef604f
Karoly Hornyak <karoly.webdev@gmail.com>
2025-12-20 22:51:04 +0000

    Update .gitignore

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.port || 8000;
+
+app.get("/", (req, res) => {
+    res.json(
+        {
+            "name": "load-track-backend",
+            "developer": {
+                "name": "Karoly Hornyak",
+                "tel": "+447421411763",
+                "email": "karoly.webdev@gmail.com",
+                "web": "karolyhornyak.com"
+            },
+        }
+    );
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/`);
+});
+
