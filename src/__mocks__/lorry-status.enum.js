2e47017799bd77b007f49595269a1178fe8d5100
Karoly Hornyak <karoly.webdev@gmail.com>
2025-12-29 08:55:43 +0000

    Create MATERIAL_NAME_ENUM for future clarity

--- src/__mocks__/lorry-status.enum.js
+++ src/__mocks__/lorry-status.enum.js
@@ -5,4 +5,24 @@ const LORRY_STATUS_ENUM = {
     CHECKED_OUT: "CHECKED_OUT"
 };
 
-module.exports = LORRY_STATUS_ENUM;
+const MATERIAL_NAME_ENUM = {
+    // Paper
+    MIXED_PAPER_GR1: "Mixed paper G1",
+    MIXED_PAPER_GR2: "Mixed paper G2",
+    OCC: "OCC",
+    NIP: "N&P",
+    TETRA: "Tetra",
+    // Plastic
+    PET_CLEAR: "Pet clear",
+    PET_COLOR: "Pet color",
+    HDPE_NATURAL: "HDPE natural",
+    HDPE_COLOR: "HDPE color",
+    MIXED_PLASTICS: "Mixed plastics",
+    // Metal
+    ALU_CANS: "Alu cans",
+    STEEL_CANS: "Steel cans",
+    // Glass
+    GLASS: "Glass"
+};
+
+module.exports = { LORRY_STATUS_ENUM, MATERIAL_NAME_ENUM };
